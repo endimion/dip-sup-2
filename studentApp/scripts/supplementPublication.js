e9e8f4296f2d9daa352b3ebe275b2af68a664deb
endimion <triantafyllou.ni@gmail.com>
2017-09-26 12:10:28 +0300

    bug fix

--- studentApp/scripts/supplementPublication.js
+++ studentApp/scripts/supplementPublication.js
@@ -6,11 +6,12 @@ function requestPublication(evt){
   let universityName = $("#uniName").val();
   let universityId = $("#uniId").val();
   let email = $("#email").val();
+  let date = $("#DateOfBirth").val();
 
   $("form").hide();
   $("#preloader").show();
 
-  $.post("/supplement/rest/request",{uniName:universityName,email:email, univId:universityId})
+  $.post("/supplement/rest/request",{uniName:universityName,email:email, univId:universityId, dateOfBirth:date})
   .then(resp =>{
       console.log(resp);
       window.location.href="/home" ;
