de95402255505b8a0a9deff146b4444692f6e643
endimion <triantafyllou.ni@gmail.com>
2017-10-04 12:43:10 +0300

    t

--- studentApp/scripts/viewAllSupplements.js
+++ studentApp/scripts/viewAllSupplements.js
@@ -210,10 +210,8 @@ function displaySupAttribute(name, value,node="<p>", properties={}){
     "background-color": "aliceblue"
   }
 
-  if(name !== "Signature" && name !== "Authorized" && name !== "Name"
-      && name != "Surname"){
-    if(typeof(value) === "string"){
-
+  if(name !== "Signature" && name !== "Authorized"){
+    if(typeof(value) === "string" && value !== "" && value){
         let result = $(node,properties);
         result.text(name+": " + value);
         if(node =="<p>"){
