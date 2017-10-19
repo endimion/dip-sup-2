a8b48dc2f183e6d28934a2b0f274b1e8f0161c3e
Nikos Triantafyllou <triantafyllou.ni@gmail.com>
2017-10-19 19:10:19 +0300

    typos

--- univApp/service/BackEndService.js
+++ univApp/service/BackEndService.js
@@ -1,10 +1,10 @@
 
 
-const PROTO_PATH = __dirname + '/protos/ds_backend_service.proto';
-let dsBackend = grpc.load(PROTO_PATH).dsbackend;
-const BACK_END = process.env.BACK_END?process.env.BACK_END||"localhost";
-const BACK_END_PORT=process.env.BACK_END_PORT?process.env.BACK_END_PORT||"50051";
-
+const PROTO_PATH = __dirname + '/../protos/ds_backend_service.proto';
+const BACK_END = process.env.BACK_END||"localhost";
+const BACK_END_PORT=process.env.BACK_END_PORT||"50051";
+const grpc = require('grpc');
+const dsBackend = grpc.load(PROTO_PATH).dsbackend;
 let client = new dsBackend.DsBackend(BACK_END+":"+BACK_END_PORT,
                                        grpc.credentials.createInsecure());
 
