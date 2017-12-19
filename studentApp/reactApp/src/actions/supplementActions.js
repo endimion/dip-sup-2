e8fffd787ab428e7d3d39c028ebc1715e2ccf53b
endimion <triantafyllou.ni@gmail.com>
2017-12-19 08:49:05 +0200

    debug

--- studentApp/reactApp/src/actions/supplementActions.js
+++ studentApp/reactApp/src/actions/supplementActions.js
@@ -14,7 +14,7 @@ export function  getSupplementsByEid(userEid) {
         axios.get("/back/supplement/rest/view")
          .then(response =>{
            // dispatch({type: "GET_SUP_FULLFILED",payload:DS})
-            dispatch({type: "GET_SUP_FULLFILED",payload:response.data})
+            dispatch({type: "GET_SUP_FULLFILED",payload:JSON.parse(response.data)})
          })
          .catch(err=>{
            dispatch({type:"GET_SUP_REJECTED",payload:err})
