2fa2fb7a6cf0403c456e84fed1ea1bdf0b60e916
endimion <triantafyllou.ni@gmail.com>
2017-12-19 09:21:19 +0200

    debug

--- studentBack/utils/authUtils.js
+++ studentBack/utils/authUtils.js
@@ -9,7 +9,7 @@ const stripchar = require('stripchar').StripChar;
 const fs = require('fs');
 const path = require('path');
 const  nJwt = require('njwt');
-const utf8 = require('utf8');
+// const utf8 = require('utf8');
 /**
   check if a user eID exists on teh session,
   if not verify the existance of a jwt token and its validity
@@ -94,12 +94,12 @@ exports.userDetailsFromToken = (req,res) =>{
         let result = JSON.parse(token.sub);
         result.eid = stripchar.RSExceptUnsAlpNum(result.eid);
         if(!result.firstName){
-          result.firstName = utf8.encode(result.currentGivenName);
+          result.firstName = result.currentGivenName;
         }
         if(!result.familyName){
-          result.familyName = utf8.encode(result.currentFamilyName);
+          result.familyName = result.currentFamilyName;
         }
-        result.userName = utf8.encode(result.firstName+"_"+result.familyName);
+        result.userName = result.firstName+"_"+result.familyName;
         resolve(result);
       }
     });
