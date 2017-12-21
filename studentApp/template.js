744899132273f0d4e46144b4084b18e89994b20e
endimion <triantafyllou.ni@gmail.com>
2017-12-21 12:47:04 +0200

    responsive

--- studentApp/template.js
+++ studentApp/template.js
@@ -6,6 +6,10 @@ export default ({ body, title,preloadedState,css}) => {
     <!DOCTYPE html>
     <html lang="en">
     <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
     	<!-- Import Google Icon Font -->
     	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     	<!-- Import materialize.css -->
