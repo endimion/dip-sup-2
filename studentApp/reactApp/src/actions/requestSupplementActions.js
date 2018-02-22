8fc0a6c96e4c720975d5d9cb09f781d0bbc2888c
Nikos <triantafyllou.ni@gmail.com>
2018-02-22 16:34:24 +0200

    UI changes

--- studentApp/reactApp/src/actions/requestSupplementActions.js
+++ studentApp/reactApp/src/actions/requestSupplementActions.js
@@ -20,9 +20,9 @@ export function requestPublication(university,universityId,email) {
      axios.post("/back/supplement/rest/request",{uniName:university,email:email,
                                           univId:universityId})
     .then(response =>{
+       $('#modal1').modal('close');
        dispatch({type:"REQUEST_PUBLISH_FULLFILED",payload:response.data})
        dispatch({type:"PUBLISH_MODAL_CLOSE"});
-        $('#modal1').modal('close');
      })
      .catch(err=>{
        dispatch({type:"REQUEST_PUBLISH_REJECTED",payload:err})
