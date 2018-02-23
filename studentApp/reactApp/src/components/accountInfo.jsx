40aa5cf960c0b7a8fe828e2d46321d2adec82e0e
Nikos <triantafyllou.ni@gmail.com>
2018-02-23 13:22:03 +0200

    css

--- studentApp/reactApp/src/components/accountInfo.jsx
+++ studentApp/reactApp/src/components/accountInfo.jsx
@@ -36,23 +36,23 @@ export default class  AccountInfo extends React.Component {
                     <form className="col s12">
                       <div className="row">
                         <div className="input-field col s12">
-                          <input id="userName" style={{margin-top: 3em;}} value={user.userName} type="text"  disabled />
+                          <input id="userName" style={{marginTop: 3em;}} value={user.userName} type="text"  disabled />
                           <label for="userName"    data-success="right">User Name</label>
                         </div>
                         <div className="input-field col s12">
-                          <input id="currentFamilyName" style={{margin-top: 3em;}} value={user.currentFamilyName} type="text" disabled />
+                          <input id="currentFamilyName" style={{marginTop: 3em;}} value={user.currentFamilyName} type="text" disabled />
                             <label for="currentFamilyName"   data-success="right">Family Name</label>
                         </div>
                         <div className="input-field col s12">
-                          <input id="currentGivenName"  style={{margin-top: 3em;}} value={user.currentGivenName} type="text" disabled />
+                          <input id="currentGivenName"  style={{marginTop: 3em;}} value={user.currentGivenName} type="text" disabled />
                             <label for="currentGivenName"   data-success="right">Given Name</label>
                         </div>
                         <div className="input-field col s12">
-                          <input id="dateOfBirth" type="text" style={{margin-top: 3em;}} value={user.dateOfBirth} disabled />
+                          <input id="dateOfBirth" type="text" style={{marginTop: 3em;}} value={user.dateOfBirth} disabled />
                             <label for="dateOfBirth"   data-success="right">Date of Birth</label>
                         </div>
                         <div className="input-field col s12">
-                          <input id="personIdentifier" type="text" style={{margin-top: 3em;}}  value={user.personIdentifier} disabled />
+                          <input id="personIdentifier" type="text" style={{marginTop: 3em;}}  value={user.personIdentifier} disabled />
                             <label for="personIdentifier" data-success="right">Person Identifier</label>
                         </div>
                       </div>
