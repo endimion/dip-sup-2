1d2892cc20618461cfeda6fd6307b3d6b162ba49
Nikos <triantafyllou.ni@gmail.com>
2018-02-28 17:30:20 +0200

    ui change

--- studentApp/reactApp/src/components/accountInfo.jsx
+++ studentApp/reactApp/src/components/accountInfo.jsx
@@ -37,31 +37,31 @@ export default class  AccountInfo extends React.Component {
      const  user = this.props.user;
       return (  <div className="main container" style={{marginTop: "3%"}}>
                   <div className="row">
-                    <div className="col s12">
+                    <h5 className="col s12">
                       Your Personal Information
-                    </div>
+                    </h5>
                   </div>
                   <div className="row">
                     <form className="col s12">
                       <div className="row">
                         <div className="input-field col s12">
-                          <input id="userName" style={{marginTop: "3em"}} value={user.userName} type="text"  disabled />
+                          <input style={{color:"black"}} id="userName" style={{marginTop: "3em"}} value={user.userName} type="text"  disabled />
                           <label for="userName"    data-success="right">User Name</label>
                         </div>
                         <div className="input-field col s12">
-                          <input id="currentFamilyName" style={{marginTop: "3em"}} value={user.currentFamilyName} type="text" disabled />
+                          <input style={{color:"black"}} id="currentFamilyName" style={{marginTop: "3em"}} value={user.currentFamilyName} type="text" disabled />
                             <label for="currentFamilyName"   data-success="right">Family Name</label>
                         </div>
                         <div className="input-field col s12">
-                          <input id="currentGivenName"  style={{marginTop: "3em"}} value={user.currentGivenName} type="text" disabled />
+                          <input style={{color:"black"}} id="currentGivenName"  style={{marginTop: "3em"}} value={user.currentGivenName} type="text" disabled />
                             <label for="currentGivenName"   data-success="right">Given Name</label>
                         </div>
                         <div className="input-field col s12">
-                          <input id="dateOfBirth" type="text" style={{marginTop: "3em"}} value={user.dateOfBirth} disabled />
+                          <input style={{color:"black"}} id="dateOfBirth" type="text" style={{marginTop: "3em"}} value={user.dateOfBirth} disabled />
                             <label for="dateOfBirth"   data-success="right">Date of Birth</label>
                         </div>
                         <div className="input-field col s12">
-                          <input id="personIdentifier" type="text" style={{marginTop: "3em"}}  value={user.personIdentifier} disabled />
+                          <input style={{color:"black"}} id="personIdentifier" type="text" style={{marginTop: "3em"}}  value={user.personIdentifier} disabled />
                             <label for="personIdentifier" data-success="right">Person Identifier</label>
                         </div>
                       </div>
@@ -74,5 +74,5 @@ export default class  AccountInfo extends React.Component {
                   </div>
                 </div>
           );
-  }
+        }
 }
