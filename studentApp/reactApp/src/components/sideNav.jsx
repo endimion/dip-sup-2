e240ac9a4781cbcc690d8208e5b8d0205da608e5
Nikos <triantafyllou.ni@gmail.com>
2018-02-28 17:59:17 +0200

    ui change

--- studentApp/reactApp/src/components/sideNav.jsx
+++ studentApp/reactApp/src/components/sideNav.jsx
@@ -38,9 +38,10 @@ export default class SideNavigation extends React.Component {
                 		}}
                 	/>
                   <SideNavItem subheader>Information</SideNavItem>
-                  <SideNavItem >{user.firstName}</SideNavItem>
-                  <SideNavItem >{user.lastName}</SideNavItem>
-                  <SideNavItem >{user.userName}</SideNavItem>
+                  <SideNavItem divider />
+                  <SideNavItem >First Name:{user.firstName}</SideNavItem>
+                  <SideNavItem >Last Name: {user.lastName}</SideNavItem>
+                  <SideNavItem >UserName: {user.userName}</SideNavItem>
                   <SideNavItem subheader>Actions</SideNavItem>
                 	<SideNavItem divider />
                 	<SideNavItem> <div onClick={e =>this.logoutUser()}>Logout</div></SideNavItem>
