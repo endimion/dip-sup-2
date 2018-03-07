9f3ac6eb3690a064ed9fd3df5d4866e3b0151a27
Nikos <triantafyllou.ni@gmail.com>
2018-03-07 13:01:18 +0200

    mail

--- studentBack/utils/emailClient.js
+++ studentBack/utils/emailClient.js
@@ -4,6 +4,7 @@ var path = require('path');
 const nodemailer = require('nodemailer');
 const fileUtils = require('./FileUtils');
 const srvUtils = require('../utils/serverUtils.js');
+const sendmail = require('sendmail')();
 
 exports.sendEmail = sendEmail;
 
@@ -20,34 +21,44 @@ function sendEmail(receiverAddress,body){
     .then( _pass => {
       // console.log("pass" + pass);
       // create reusable transporter object using the default SMTP transport
-      let transporter = nodemailer.createTransport({
-       // service: 'gmail',
-        host: 'mail.programmerdiaries.com',
-        port: '25',
-//      secure: false,
-        // tls:{
-          // rejectUnauthorized: false
-        // },
-        auth: {
-          user: 'dsservice@programmerdiaries.com',
-          // pass: _pass
-          // user: 'user@dss.aegean.gr',
-          pass: 'D$d1pl0m@'
-        }
-      });
-
-      console.log("transporter");
-      console.log(transporter);
-      console.log( 'Email Body ' + body );
-      // setup email data with unicode symbols
-      let mailOptions = {
-        from: '"Diploma Supplement Service" <supplements@ds.service.aegean.gr>', // sender address
-        to: receiverAddress,// list of receivers
-        subject: 'A Diploma Supplement has been shared with you ', // Subject line
-        text: body,//'Hello world ?', // plain text body
-        html: body //Hello world ?</b>' // html body
-      };
-        transporter.sendMail(mailOptions)
+//       let transporter = nodemailer.createTransport({
+//        // service: 'gmail',
+//         host: 'mail.programmerdiaries.com',
+//         port: '25',
+// //      secure: false,
+//         // tls:{
+//           // rejectUnauthorized: false
+//         // },
+//         auth: {
+//           user: 'dsservice@programmerdiaries.com',
+//           // pass: _pass
+//           // user: 'user@dss.aegean.gr',
+//           pass: 'D$d1pl0m@'
+//         }
+//       });
+//
+//       console.log("transporter");
+//       console.log(transporter);
+//       console.log( 'Email Body ' + body );
+//       // setup email data with unicode symbols
+//       let mailOptions = {
+//         from: '"Diploma Supplement Service" <supplements@ds.service.aegean.gr>', // sender address
+//         to: receiverAddress,// list of receivers
+//         subject: 'A Diploma Supplement has been shared with you ', // Subject line
+//         text: body,//'Hello world ?', // plain text body
+//         html: body //Hello world ?</b>' // html body
+//       };
+//         transporter.sendMail(mailOptions)
+//
+        sendmail({
+            from: 'no-reply@dss.aegean.gr',
+            to: receiverAddress,
+            subject: 'test sendmail',
+            html: body,
+          }, function(err, reply) {
+            console.log(err && err.stack);
+            console.dir(reply);
+        })
         .then(result => {
                 console.log(`mail sent ${result}`);
                 console.log(result);
