001b74c5ad85b35be85abc061fca9b15776c5bda
Nikos <triantafyllou.ni@gmail.com>
2018-03-09 09:09:21 +0200

    email server

--- studentBack/utils/emailClient.js
+++ studentBack/utils/emailClient.js
@@ -22,59 +22,67 @@ function sendEmail(receiverAddress,body){
     .then( _pass => {
       // console.log("pass" + pass);
       // create reusable transporter object using the default SMTP transport
-//       let transporter = nodemailer.createTransport({
-//        // service: 'gmail',
-//         host: 'mail.programmerdiaries.com',
-//         port: '25',
-// //      secure: false,
-//         // tls:{
-//           // rejectUnauthorized: false
-//         // },
-//         auth: {
-//           user: 'dsservice@programmerdiaries.com',
-//           // pass: _pass
-//           // user: 'user@dss.aegean.gr',
-//           pass: 'D$d1pl0m@'
-//         }
-//       });
+      let transporter = nodemailer.createTransport({
+       // service: 'gmail',
+        host: ' smtp.aegean.gr',
+        port: '587',
+        secure: true,
+        tls:{
+          rejectUnauthorized: false
+        },
+        auth: {
+          user: 'dss@aegean.gr',
+          // pass: _pass
+          // user: 'user@dss.aegean.gr',
+          pass: 'ddd111!!!'
+        }
+      });
 //
 //       console.log("transporter");
 //       console.log(transporter);
 //       console.log( 'Email Body ' + body );
 //       // setup email data with unicode symbols
-//       let mailOptions = {
-//         from: '"Diploma Supplement Service" <supplements@ds.service.aegean.gr>', // sender address
-//         to: receiverAddress,// list of receivers
-//         subject: 'A Diploma Supplement has been shared with you ', // Subject line
-//         text: body,//'Hello world ?', // plain text body
-//         html: body //Hello world ?</b>' // html body
-//       };
-//         transporter.sendMail(mailOptions)
+      let mailOptions = {
+        from: '"Diploma Supplement Service" <supplements@ds.service.aegean.gr>', // sender address
+        to: receiverAddress,// list of receivers
+        subject: 'A Diploma Supplement has been shared with you ', // Subject line
+        // text: body,//'Hello world ?', // plain text body
+        html: body //Hello world ?</b>' // html body
+      };
+        transporter.sendMail(mailOptions)
+        .then(result => {
+                console.log(`mail sent ${result}`);
+                console.log(result);
+                resolve(result);
+        })
+        .catch(err => {
+                console.log(err);
+                reject(err)
+        });
+
+//           process.env.subject='A Diploma Supplement has been shared with you '
+//           process.env.from="no-reply@dss.aegean.gr"
+//           process.env.recipients=receiverAddress
+//           process.env.body=body
+//           console.log()
+//           // process.env.mail="subject:$subject\nfrom:$from\n$body"
+//                     exec(`echo "From: $from
+// To: $recipients
+// MIME-Version: 1.0
+// Content-Type: text/html
+// Subject: $subject
+// $body
+// " | sendmail -t `, (err, stdout, stderr) => {
+//                       if (err) {
+//                         // node couldn't execute the command
+//                         return;
+//                       }
 //
-
-          process.env.subject='A Diploma Supplement has been shared with you '
-          process.env.from="no-reply@dss.aegean.gr"
-          process.env.recipients=receiverAddress
-          process.env.body=body
-          console.log()
-          // process.env.mail="subject:$subject\nfrom:$from\n$body"
-                    exec(`echo "From: $from
-To: $recipients
-MIME-Version: 1.0
-Content-Type: text/html
-Subject: $subject
-$body
-" | sendmail -t `, (err, stdout, stderr) => {
-                      if (err) {
-                        // node couldn't execute the command
-                        return;
-                      }
-
-                      // the *entire* stdout and stderr (buffered)
-                      console.log(`stdout: ${stdout}`);
-                      console.log(`stderr: ${stderr}`);
-                      resolve();
-                    });
+//                       // the *entire* stdout and stderr (buffered)
+//                       console.log(`stdout: ${stdout}`);
+//                       console.log(`stderr: ${stderr}`);
+//                       resolve();
+//                     });
 
         // sendmail({
         //     from: 'no-reply@dss.aegean.gr',
@@ -85,15 +93,6 @@ $body
         //     console.log(err && err.stack);
         //     console.dir(reply);
         // })
-        // .then(result => {
-        //         console.log(`mail sent ${result}`);
-        //         console.log(result);
-        //         resolve(result);
-        // })
-        // .catch(err => {
-        //         console.log(err);
-        //         reject(err)
-        // });
     })
     .catch(err => {reject(err);})
   });
