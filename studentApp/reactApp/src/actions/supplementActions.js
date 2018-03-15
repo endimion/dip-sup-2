a80429affa79b73e1a41a410f8cbb573ab32c594
Nikos <triantafyllou.ni@gmail.com>
2018-03-15 18:05:38 +0200

    agri uni changes

--- studentApp/reactApp/src/actions/supplementActions.js
+++ studentApp/reactApp/src/actions/supplementActions.js
@@ -4,7 +4,7 @@ import DS from "../assets/fakeDS"
 import axios from "axios"
 // ES6
 import axiosRetry from 'axios-retry';
-import retryAxiosNtimes from 'util'
+import retryAxiosNtimes from 'utils.js'
 
 
 axiosRetry(axios, { retries: 3 });
