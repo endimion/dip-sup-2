cde90db88787a52f53b5cf7914078a0625269c03
Nikos <triantafyllou.ni@gmail.com>
2018-03-15 20:11:05 +0200

    agri

--- studentAppAgri/appOld.js
+++ studentAppAgri/appOld.js
@@ -4,7 +4,7 @@
 
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8010;
 const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session'); //warning The default server-side session storage, MemoryStore, is purposely not designed for a production environment.
